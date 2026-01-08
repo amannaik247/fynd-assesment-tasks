ef9f197614c9cda809a81ec52973889c6f5f3c01
Aman Naik <168978808+amannaik247@users.noreply.github.com>
2026-01-08 19:58:17 +0530

    Remove: unwanted files of task2

--- task2/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import './globals.css';
-export const metadata = { title: 'Fynd Feedback', description: 'User feedback form' };
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" className="bg-black text-white">
-      <body>{children}</body>
-    </html>
-  );
-}
